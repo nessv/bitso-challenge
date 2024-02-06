d91939f50682b708022201b57a40e724dd9085a3
Néstor Valdez <nvaldez@Nestors-Mac-Studio.local>
2024-02-06 18:00:04 -0300

    Initial Commit

---
+++ Bitso/BitsoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitsoApp.swift
+//  Bitso
+//
+//  Created by Néstor Valdez on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BitsoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
